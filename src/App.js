7f224ee7e0c29f97bf8b2ebcf887b90089508242
Judith Osei-Tete <jao92@cornell.edu>
2024-07-24 21:49:35 -0400

    create layout for main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./main.css";
+
+function Main() {
+  return (
+    <>
+      <section className="main-section" id="section1">
+        <div className="earths-surface">EARTH'S SURFACE</div>
+
+        <div className="intro">
+          <h1>Ascend</h1>
+          <h2>Made by Judith Osei-Tete</h2>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Main;
