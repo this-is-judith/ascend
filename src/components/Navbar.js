3bdb40421aee15b787783db8f4cbbb5d88d9d815
Judith Osei-Tete <jao92@cornell.edu>
2024-07-24 20:35:01 -0400

    add icons and position nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import "./navbar.css";
 import logo from "../photos/ascend-logo.png";
+import { GiJumpingRope } from "react-icons/gi";
+import { FaGaugeSimpleHigh } from "react-icons/fa6";
+import { FaThermometerHalf, FaTape } from "react-icons/fa";
+import { TbRuler2, TbRuler2Off } from "react-icons/tb";
 
 function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
@@ -10,10 +14,6 @@ function Navbar() {
   return (
     <>
       <nav className="navbar">
-        <a href="#" className="nav-logo link" onClick={toggleShowMenu}>
-          <img src={logo} alt="Ascend Logo" />
-        </a>
-
         <div
           className={
             showMenu ? "navbar-container show-menu" : "navbar-container"
@@ -21,31 +21,40 @@ function Navbar() {
         >
           {/* Jump */}
           <div className="navbar-item">
-            <div className="navbar-item-icon"></div>
+            <div>
+              <GiJumpingRope className="navbar-item-icon" />
+            </div>
             <div className="navbar-item-label">JUMP</div>
           </div>
 
           {/* Pressure */}
           <div className="navbar-item">
-            <div className="navbar-item-icon"></div>
+            <div className="navbar-item-icon">
+              <FaGaugeSimpleHigh />
+            </div>
             <div className="navbar-item-label">PRESSURE</div>
           </div>
 
           {/* Temperature */}
           <div className="navbar-item">
-            <div className="navbar-item-icon"></div>
+            <div className="navbar-item-icon">
+              <FaThermometerHalf />
+            </div>
             <div className="navbar-item-label">TEMPERATURE</div>
           </div>
 
           {/* Units */}
           <div className="navbar-item">
-            <div className="navbar-item-icon"></div>
+            <div className="navbar-item-icon">
+              <FaTape />
+            </div>
             <div className="navbar-item-label">UNITS</div>
           </div>
 
           {/* Height Line */}
           <div className="navbar-item">
             <div className="navbar-item-icon">
+              <TbRuler2 />
               {/* onClick={handleClick} */}
               {/* className={click ? "height-line" : "crossed-out-height-line"} */}
             </div>
@@ -61,6 +70,10 @@ function Navbar() {
             <div className="navbar-item-label">DARK</div>
           </div>
         </div>
+
+        <a href="#" className="nav-logo link" onClick={toggleShowMenu}>
+          <img src={logo} alt="Ascend Logo" />
+        </a>
       </nav>
     </>
   );
