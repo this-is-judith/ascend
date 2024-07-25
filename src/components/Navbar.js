339d6bd711aeee7fa2df002fb3437e80e050d5f8
Judith Osei-Tete <jao92@cornell.edu>
2024-07-24 21:33:48 -0400

    configure navbar position and logo toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,75 +1,77 @@
 import React, { useState } from "react";
 import "./navbar.css";
+
 import logo from "../photos/ascend-logo.png";
-import { GiJumpingRope } from "react-icons/gi";
+import { GiJumpAcross } from "react-icons/gi";
 import { FaGaugeSimpleHigh } from "react-icons/fa6";
-import { FaThermometerHalf, FaTape } from "react-icons/fa";
+import { FaThermometerHalf } from "react-icons/fa";
+import { MdViewColumn } from "react-icons/md";
 import { TbRuler2, TbRuler2Off } from "react-icons/tb";
+import { LiaToggleOffSolid, LiaToggleOnSolid } from "react-icons/lia";
 
 function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
 
-  const toggleShowMenu = () => setShowMenu(!setShowMenu);
+  const toggleShowMenu = () => setShowMenu(!showMenu);
 
   return (
     <>
       <nav className="navbar">
-        <div
-          className={
-            showMenu ? "navbar-container show-menu" : "navbar-container"
-          }
-        >
-          {/* Jump */}
-          <div className="navbar-item">
-            <div>
-              <GiJumpingRope className="navbar-item-icon" />
+        {showMenu && (
+          <div className="navbar-container">
+            {/* Jump */}
+            <div className="navbar-item">
+              <div className="navbar-item-icon jump">
+                <GiJumpAcross />
+              </div>
+              <div className="navbar-item-label">JUMP</div>
             </div>
-            <div className="navbar-item-label">JUMP</div>
-          </div>
 
-          {/* Pressure */}
-          <div className="navbar-item">
-            <div className="navbar-item-icon">
-              <FaGaugeSimpleHigh />
+            {/* Pressure */}
+            <div className="navbar-item">
+              <div className="navbar-item-icon pressure">
+                <FaGaugeSimpleHigh />
+              </div>
+              <div className="navbar-item-label">PRESSURE</div>
             </div>
-            <div className="navbar-item-label">PRESSURE</div>
-          </div>
 
-          {/* Temperature */}
-          <div className="navbar-item">
-            <div className="navbar-item-icon">
-              <FaThermometerHalf />
+            {/* Temperature */}
+            <div className="navbar-item">
+              <div className="navbar-item-icon temperature">
+                <FaThermometerHalf />
+              </div>
+              <div className="navbar-item-label">TEMPERATURE</div>
             </div>
-            <div className="navbar-item-label">TEMPERATURE</div>
-          </div>
 
-          {/* Units */}
-          <div className="navbar-item">
-            <div className="navbar-item-icon">
-              <FaTape />
+            {/* Units */}
+            <div className="navbar-item">
+              <div className="navbar-item-icon units">
+                <MdViewColumn />
+              </div>
+              <div className="navbar-item-label">UNITS</div>
             </div>
-            <div className="navbar-item-label">UNITS</div>
-          </div>
 
-          {/* Height Line */}
-          <div className="navbar-item">
-            <div className="navbar-item-icon">
-              <TbRuler2 />
-              {/* onClick={handleClick} */}
-              {/* className={click ? "height-line" : "crossed-out-height-line"} */}
+            {/* Height Line */}
+            <div className="navbar-item">
+              <div className="navbar-item-icon height-line">
+                <TbRuler2 />
+                {/* onClick={handleClick} */}
+                {/* className={click ? "height-line" : "crossed-out-height-line"} */}
+              </div>
+              <div className="navbar-item-label">HEIGHT LINE</div>
             </div>
-            <div className="navbar-item-label">HEIGHT LINE</div>
-          </div>
 
-          {/* Dark/Light Mode */}
-          <div className="navbar-item">
-            <div className="navbar-item-icon">
-              {/* onClick={handleClick} */}
-              {/* className={click ? "height-line" : "crossed-out-height-line"} */}
+            {/* Dark/Light Mode */}
+            <div className="navbar-item">
+              <div className="navbar-item-icon dark-light-mode">
+                <LiaToggleOffSolid />
+                {/* onClick={handleClick} */}
+                {/* className={click ? "height-line" : "crossed-out-height-line"} */}
+              </div>
+              <div className="navbar-item-label">DARK</div>
             </div>
-            <div className="navbar-item-label">DARK</div>
           </div>
-        </div>
+        )}
 
         <a href="#" className="nav-logo link" onClick={toggleShowMenu}>
           <img src={logo} alt="Ascend Logo" />
