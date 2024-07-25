123e50883f2a76bb6bb4f636c490c415945f0fa4
Judith Osei-Tete <jao92@cornell.edu>
2024-07-25 16:34:47 -0400

    remove welcome message and configure sizes

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,8 +17,8 @@ function Main() {
           <div className="grass-right"></div>
         </div>
 
-        <div className="intro crust">
-          <div className="ace-animation">
+        <div className="intro crust-gradient">
+          <div className="ace-container">
             <div className="ace-left"></div>
             <div className="ace-cage">
               <FaPerson className="ace" />
@@ -27,20 +27,14 @@ function Main() {
           </div>
 
           <div className="title">
-            <h1>Ascend</h1>
+            <h1>ASCEND</h1>
           </div>
 
           <div className="subtitle">
-            <h2>Made by Judith Osei-Tete</h2>
+            <h2>made by Judith Osei-Tete</h2>
           </div>
 
-          <div className="welcome">
-            <p>
-              Welcome! This is an interactive page showing the journey from
-              Earth's surface to the edge of space (and beyond!), revealing the
-              heights of objects and phenomena along the way.
-            </p>
-          </div>
+          <div className="instructions">Instructions</div>
         </div>
       </section>
     </>
