860c8f195fb0a91f014625c6577551cfdaf932b5
Judith Osei-Tete <jao92@cornell.edu>
2024-07-26 14:59:31 -0400

    restructure nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,10 @@ import "./navbar.css";
 
 import logo from "../photos/ascend-logo.png";
 import { GiJumpAcross } from "react-icons/gi";
-import { FaGaugeSimpleHigh } from "react-icons/fa6";
-import { FaThermometerHalf } from "react-icons/fa";
 import { MdViewColumn } from "react-icons/md";
 import { TbRuler2, TbRuler2Off } from "react-icons/tb";
 import { LiaToggleOffSolid, LiaToggleOnSolid } from "react-icons/lia";
+import { FaCircleInfo } from "react-icons/fa6";
 
 function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
@@ -20,27 +19,11 @@ function Navbar() {
         {showMenu && (
           <div className="navbar-container">
             {/* Jump */}
-            <div className="navbar-item">
-              <div className="navbar-item-icon jump">
+            <div className="navbar-item jump">
+              <div className="navbar-item-icon jump-icon">
                 <GiJumpAcross />
               </div>
               <div className="navbar-item-label">JUMP</div>
-            </div>
-
-            {/* Pressure */}
-            <div className="navbar-item">
-              <div className="navbar-item-icon pressure">
-                <FaGaugeSimpleHigh />
-              </div>
-              <div className="navbar-item-label">PRESSURE</div>
-            </div>
-
-            {/* Temperature */}
-            <div className="navbar-item">
-              <div className="navbar-item-icon temperature">
-                <FaThermometerHalf />
-              </div>
-              <div className="navbar-item-label">TEMP</div>
             </div>
 
             {/* Units */}
@@ -58,12 +41,20 @@ function Navbar() {
                 {/* onClick={handleClick} */}
                 {/* className={click ? "height-line" : "crossed-out-height-line"} */}
               </div>
-              <div className="navbar-item-label">HEIGHT LINE</div>
+              <div className="navbar-item-label">HEIGHT</div>
+            </div>
+
+            {/* Info */}
+            <div className="navbar-item info">
+              <div className="navbar-item-icon">
+                <FaCircleInfo />
+              </div>
+              <div className="navbar-item-label">INFO</div>
             </div>
 
             {/* Dark/Light Mode */}
-            <div className="navbar-item">
-              <div className="navbar-item-icon dark-light-mode">
+            <div className="navbar-item dark">
+              <div className="navbar-item-icon dark-icon">
                 <LiaToggleOffSolid />
                 {/* onClick={handleClick} */}
                 {/* className={click ? "height-line" : "crossed-out-height-line"} */}
