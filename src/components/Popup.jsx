54174ed7ed60046b116662293323ab19dbe22d01
Judith Osei-Tete <jao92@cornell.edu>
2024-07-26 20:05:00 -0400

    finish tutorial for iphone se

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -11,14 +11,17 @@ const Popup = ({ onClose }) => {
           <p>
             <strong>Ascend</strong> is an interactive page that takes you on a
             journey from the Earth's surface to the edge of space, comparing the
-            heights of objects along the way.
+            heights of objects along the way. Unlike most websites, you don't
+            scroll down for content, you scroll up.
           </p>
+
+          <p className="bolder-font">You have two choices:</p>
         </div>
 
         <div className="content-item option option-1">
           <div className="option-1-1">1</div>
           <div className="option-1-2">
-            <p>You can fly automatically with Ace</p>
+            <p>Fly automatically with Ace</p>
           </div>
         </div>
 
@@ -26,8 +29,8 @@ const Popup = ({ onClose }) => {
           <div className="option-2-1">2</div>
           <div className="option-2-2">
             <p>
-              Or scroll up yourself without Ace. Get ready for 10,000+ km of
-              content though!
+              Scroll up manually. It's a lot, so get ready for 10,000+ km of
+              content!
             </p>
           </div>
         </div>
@@ -37,38 +40,6 @@ const Popup = ({ onClose }) => {
           <img src={Menu} alt="Menu Button" className="menu-button" /> to reveal
           some features you can use.
         </p>
-
-        <div className="content-item option menu-tutorial">
-          <div className="menu-item">
-            <div className="left">L</div>
-            <div className="middle">M</div>
-            <div className="right">R</div>
-          </div>
-
-          <div className="menu-item">
-            <div className="left">L</div>
-            <div className="middle">M</div>
-            <div className="right">R</div>
-          </div>
-
-          <div className="menu-item">
-            <div className="left">L</div>
-            <div className="middle">M</div>
-            <div className="right">R</div>
-          </div>
-
-          <div className="menu-item">
-            <div className="left">L</div>
-            <div className="middle">M</div>
-            <div className="right">R</div>
-          </div>
-
-          <div className="menu-item">
-            <div className="left">L</div>
-            <div className="middle">M</div>
-            <div className="right">R</div>
-          </div>
-        </div>
 
         <div className="content-item button-container">
           <button onClick={onClose} className="close-button">
